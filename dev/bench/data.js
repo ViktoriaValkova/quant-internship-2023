b80921d90fad1a7a665f30fa1847a4dbeb6ca4b3
github-action-benchmark <github@users.noreply.github.com>
2024-01-10 13:19:14 +0000

    add My Project Go Benchmark (go) benchmark result for 2fc1acf5115736768822afea82d7734ddb79c2f3

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1704892754355,
+  "repoUrl": "https://github.com/ViktoriaValkova/quant-internship-2023",
+  "entries": {
+    "My Project Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "132173101+ViktoriaValkova@users.noreply.github.com",
+            "name": "Viktoria Valkova",
+            "username": "ViktoriaValkova"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2fc1acf5115736768822afea82d7734ddb79c2f3",
+          "message": "Update benchmark.yml",
+          "timestamp": "2024-01-10T14:18:22+01:00",
+          "tree_id": "7827fcfc6250948fe5d6c95f331c27b32eeb4f33",
+          "url": "https://github.com/ViktoriaValkova/quant-internship-2023/commit/2fc1acf5115736768822afea82d7734ddb79c2f3"
+        },
+        "date": 1704892753175,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetGifsHandler",
+            "value": 253263,
+            "unit": "ns/op",
+            "extra": "4566 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
